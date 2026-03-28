88e246aa36a685e26dc5b587d96ff089ede9da71
Henry <henry@mac.home>
2026-03-28 20:28:58 -0300

    full load for fact modeling  setup

---
+++ notebooks/3_fact_modeling/1_full_load_fact.py
@@ -0,0 +1,37 @@
+# Databricks notebook source
+from pyspark.sql import functions as F
+from delta.tables import DeltaTable
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC **Load Project Utilities & Initialize Notebook Widgets**
+
+# COMMAND ----------
+
+# MAGIC %run /Workspace/Users/lhungen@gmail.com/FMCG_pipeline/1_setup/utilities
+
+# COMMAND ----------
+
+print(bronze_schema, silver_schema, gold_schema)
+
+# COMMAND ----------
+
+dbutils.widgets.text("catalog", "fmcg", "Catalog")
+dbutils.widgets.text("data_source", "gross_price", "Data Source")
+
+catalog = dbutils.widgets.get("catalog")
+data_source = dbutils.widgets.get("data_source")
+
+base_path = f's3://fmcg-databricks-bronze/{data_source}'
+landing_path = f"{base_path}/landing"
+processed_path = f"{base_path}/processed"
+print(base_path)
+print(landing_path)
+print(processed_path)
+
+#define the tables
+bronze_table = f"{catalog}.{bronze_schema}.{data_source}"
+silver_table = f"{catalog}.{silver_schema}.{data_source}"
+gold_table = f"{catalog}.{gold_schema}.{data_source}"
+
